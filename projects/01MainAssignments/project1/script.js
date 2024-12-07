e647e90bfb963b3e9ac56ab3703e25e2716a1a6d
scrymx <ojutzet@zhdk.ch>
2024-12-07 17:48:48 +0100

    implemented code

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,158 @@
-let len = 10;
-let bg;
+let tvIMG;
+let buttonIMG;
+let channelIMG;
+let font;
+let font2;
+let tv;
+let points;
+let bounds;
+let TVOn = false;
+let CHOn = false;
+let button;
+let channelButton;
+let editibleString = "type something...";
+let pointcolor = 0;
+
+// scaling font
+let speed = 0.008;
+let amplitude = 0.7;
+
+function preload() {
+  tvIMG = loadImage("assets/images/tv.png");
+  font = loadFont("assets/fonts/Roboto-Bold.ttf");
+  font2 = loadFont("assets/fonts/Tiny5-Regular.ttf");
+}
+
+class Tv {
+  constructor(x, y, color, IMG) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.IMG = IMG;
+  }
+  display() {
+    image(this.IMG, 0, 0, width, height);
+  }
+}
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+  createCanvas(windowWidth, windowHeight);
+
+  // red button
+  button = createImg("assets/images/button_red.png", "red button");
+  button.size(60, 60);
+  button.position(20, windowHeight / 2 - 30);
+  button.mousePressed(toggleTV);
+
+  // channel buttons
+  channelButton = createImg("assets/images/button_channels.png", "channel buttons");
+  channelButton.size(60, 120);
+  channelButton.position(20, windowHeight / 2 + 50);
+  channelButton.mousePressed(toggleCH);
+
+  tv = new Tv(0, 0, 0, tvIMG);
+
+  // point text
+  points = font.textToPoints("iAD TV", 0, 0, 10, {
+    sampleFactor: 5,
+    simplifyThreshold: 0,
+  });
+  bounds = font.textBounds("iAD TV", 0, 0, 10);
+
+  textFont(font2);
+  textSize(width / 3);
+  textAlign(CENTER, CENTER);
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
-	}
+function draw() {
+  textAlign(CENTER, CENTER);
+  background(230);
+
+  adjustButtonSizes();
+
+  if (!CHOn) {
+    push();
+    fill(pointcolor);
+    let scaleFactor = sin(frameCount * speed) * amplitude;
+    let scaleW = (width / bounds.w) * scaleFactor;
+    let scaleH = (height / bounds.h) * scaleFactor;
+    let w = bounds.w * scaleW;
+    let h = bounds.h * scaleH;
+    let fontX = -bounds.x * scaleW + width / 2 - w / 2;
+    let fontY = -bounds.y * scaleH + height / 2 - h / 2;
+    translate(fontX, fontY);
+    for (let i = 0; i < points.length; i++) {
+      let p = points[i];
+      let X = mouseX * random(0.01) + p.x * scaleW;
+      let Y = random(4) + p.y * scaleH;
+      rect(X, Y, 8, 8);
+    }
+    pop();
+		
+		
+  } else {
+    background(0, 255, 0);
+    fill(0);
+    let sizef = height * 0.04;
+    textSize(sizef);
+    for (let y = 0; y < height / sizef; y++) {
+      let frequency = radians(frameCount * 3);
+      let offset = y * 0.9;
+      let amplitude = mouseX * 0.03;
+      let x = width / 2 + sin(frequency + offset) * amplitude;
+      text(editibleString, x, y * sizef);
+    }
+  }
+
+  if (!TVOn) {
+    fill(0);
+    rect(0, 0, width, height);
+  }
+
+  tv.display();
 }
 
+// responsive button
+function adjustButtonSizes() {
+  if (windowWidth <= 1200) {
+    button.size(30, 30);
+		button.position(windowWidth/1.5, windowHeight * 0.95);
+    channelButton.size(30, 60);
+		channelButton.position(windowWidth/2, windowHeight * 0.93);
+		channelButton.style("transform", "rotate(90deg)");
+  } else {
+    button.size(60, 60);
+    channelButton.size(60, 120);
+  }
+}
+
+// toggle on/off
+function toggleTV() {
+  TVOn = !TVOn;
+}
+
+// change channel
+function toggleCH() {
+  CHOn = !CHOn;
+}
+
+function keyTyped() {
+  if (keyCode == ENTER) {
+    editibleString = "";
+  } else {
+    editibleString = editibleString + key;
+  }
+}
+
+function mouseClicked() {
+  if (pointcolor === 0) {
+    pointcolor = 255;
+  } else {
+    pointcolor = 0;
+  }
+}
+
+// responsive
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -1,29 +1,68 @@
-let len = 10;
-let bg;
+let sliderR, sliderG, sliderB;
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+  createCanvas(windowWidth, windowHeight);
+
+  // sliders
+  sliderR = createSlider(0, 255, 127);
+  sliderG = createSlider(0, 255, 127);
+  sliderB = createSlider(0, 255, 127);
+
+  sliderR.position(width / 2 - 100, height / 2 - 50);
+  sliderG.position(width / 2 - 100, height / 2);
+  sliderB.position(width / 2 - 100, height / 2 + 50);
+
+  // styles
+  sliderR.style('width', '200px');
+  sliderG.style('width', '200px');
+  sliderB.style('width', '200px');
+	
+	sliderR.style('border-radius', '50px');
+  sliderG.style('border-radius', '50px');
+  sliderB.style('border-radius', '50px');
+
+  sliderR.style('background', 'rgb(255,0,0)');
+  sliderG.style('background', 'rgb(0,255,0)');
+  sliderB.style('background', 'rgb(0,0,255)');
+	
+	
+	sliderR.class('customSlider redSlider');
+  sliderG.class('customSlider greenSlider');
+  sliderB.class('customSlider blueSlider');
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
-	}
-}
+function draw() {
+  let r = sliderR.value();
+  let g = sliderG.value();
+  let b = sliderB.value();
 
+  background(r, g, b);
+
+  sliderR.input(() => {
+    let offsetR = sliderR.value();
+    sliderG.value(constrain(offsetR - 100, 0, 130));
+    sliderB.value(map(offsetR, 130, 50, 50, 130));
+  });
+
+  sliderG.input(() => {
+    let offsetG = sliderG.value();
+    sliderR.value(constrain(offsetG * 1.5, 0, 255));
+    sliderB.value(map(offsetG, 130, 50, 50, 130));
+  });
+
+  sliderB.input(() => {
+    let offsetB = sliderB.value();
+    sliderR.value(map(offsetB, 130, 50, 50, 130));
+    sliderG.value(constrain(offsetB * 0.3, 0, 255));
+  });
+
+	//text
+  textAlign(CENTER);
+  textSize(22);
+  fill(255);
+ 	text(`change the background color to RGB: 0, 255, 0`, width / 2, height / 2 + 120);
+	textSize(15);
+	text(`R: ${r}`, width / 2 , height / 2 + 150);
+  text(`G: ${g}`, width / 2, height / 2 + 180);
+  text(`B: ${b}`, width / 2, height / 2 + 210);
+}
